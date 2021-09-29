9fe08a67ff3ba48c412686edd933344b3d9c7470
ChAuLaProduction <86785480+ChAuLaProduction@users.noreply.github.com>
2021-09-29 22:43:50 +0530

    Update help.py

--- bot/plugins/help.py
+++ bot/plugins/help.py
@@ -11,19 +11,20 @@ Hi {mention}. Welcome to Screenshot Generator Bot. You can use me to generate:
     2. Sample Video.
     3. Trim Video.
 
-👉 I support any kind of **telegram video file** (streaming video or document video files) provided it --has proper mime-type-- and --is not corrupted--.
-👉 I also support **Streaming URLs**. The URL should be a --streaming URL--, --non IP specific--, and --should return proper response codes--.
-Just send me the telegram file or the streaming URL.
+• I support any kind of **telegram video file** (streaming video or document video files).
+• I also support **Streaming URLs**. The URL should be a --streaming URL--, --non IP specific--, and --should return proper response codes--.
+
+Just Send The Telegram file or the streaming URL.
 
 See /settings to configure bot's behavior.
 Use /set_watermark to set custom watermarks to your screenshots.
 
 **General FAQ.**
 
-👉 If the bot dosen't respond to telegram files you forward, first check /start and --confirm bot is alive--. Then make sure the file is a **video file** which satisfies above mentioned conditions.
-👉 If bot replies __😟 Sorry! I cannot open the file.__, the file might be --currupted-- or --is malformatted--.
+• If the bot dosen't respond to telegram files you forward, first check /start and --confirm bot is alive--.
+• If bot replies ' Can't Open The File ', the file might be --currupted-- or --malformatted--.
 
-__If issues persists contact my father.__
+__If Issues Persists , Can't Do Anything Just Search For another Bot.__
 
 {admin_notification}
 """
